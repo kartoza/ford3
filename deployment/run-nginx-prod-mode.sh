bee447fdc71bd4f6a8b52c9f2713a3b95de5ea0e
Christiaanvdm <Christiaan@kartoza.com>
2019-01-22 09:31:48 +0200

    Removed deployment from git tracked files

--- deployment/run-nginx-prod-mode.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti -v /Users/timlinux/dev/python/projecta/deployment/static/:/home/web/static/ --link projecta-uwsgi -p 8889:80 -v /Users/timlinux/dev/python/projecta/deployment/media/:/home/web/media/ kartoza/nginx-django
