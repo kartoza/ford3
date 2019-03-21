774c52d392f04961e9a5cc7244ba9b5ebb2bdd39
Christiaanvdm <Christiaan@kartoza.com>
2019-03-21 20:12:07 +0200

    ensured too long error is raised for telephone

--- django_project/ford3/forms/provider_form.py
+++ django_project/ford3/forms/provider_form.py
@@ -2,18 +2,21 @@ from django import forms
 from ford3.models.provider import Provider
 
 
-EMPTY_TEL_ERROR = 'Please enter your telephone number'
+EMPTY_TEL_ERROR = 'Your telephone number is required.'
+EMPTY_EMAIL_ERROR = 'Your email is required.'
 
 
 class ProviderForm(forms.models.ModelForm):
 
     class Meta:
         model = Provider
-        fields = ('telephone',)
+        fields = ('telephone', 'email')
         widgets = {
             'telephone': forms.fields.TextInput(
-                attrs={'placeholder': '••• ••• ••••'})
+                attrs={'placeholder': '••• ••• ••••'}),
+            'email':  forms.fields.EmailInput()
         }
         error_messages = {
-            'telephone': {'required': EMPTY_TEL_ERROR}
+            'telephone': {'required': EMPTY_TEL_ERROR},
+            'email' : {'required': EMPTY_EMAIL_ERROR}
         }
--- django_project/ford3/tests/forms/test_provider_form.py
+++ django_project/ford3/tests/forms/test_provider_form.py
@@ -1,18 +1,31 @@
 from django.test import TestCase
-from ford3.forms.provider_form import ProviderForm, EMPTY_TEL_ERROR
+from ford3.forms.provider_form import ProviderForm, EMPTY_TEL_ERROR, EMPTY_EMAIL_ERROR
 
 
 class ProviderFormTest(TestCase):
 
     def test_form_validation_for_blank_items(self):
-        form = ProviderForm(data={'telephone': ''})
+        form = ProviderForm(
+            data={
+                'telephone': '',
+                'email': '',
+            })
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['telephone'], [EMPTY_TEL_ERROR])
+        self.assertEqual(form.errors['email'], [EMPTY_EMAIL_ERROR])
 
     def test_provider_page_uses_provider_form(self):
         response = self.client.get('/ProviderForm/')
         self.assertIsInstance(response.context['form'], ProviderForm)
 
     def test_form_validation_for_max_length(self):
-        form = ProviderForm(data={'telephone': '0821234123412341234'})
+        form = ProviderForm(
+            data={'telephone': '0821234123412341234',
+                  'email': 'somestupidlylongemailaddressthatshouldntpass'
+                               '@anytest.com'})
         self.assertFalse(form.is_valid())
+        try:
+            self.assertIn('Ensure this value has at most 12 characters',
+                          str(form.errors['telephone'][0]))
+        except KeyError:
+            self.fail(msg='No error raised for telephone being long.')
