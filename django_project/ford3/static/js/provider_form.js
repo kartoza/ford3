0a4b7df1f481e8d4bab117ad61c83448e10f729f
Christiaanvdm <Christiaan@kartoza.com>
2019-03-27 11:52:32 +0200

    Replaced string literals with appended strings

--- django_project/ford3/static/js/provider_form.js
+++ django_project/ford3/static/js/provider_form.js
@@ -27,17 +27,21 @@ function createCampusNameInputs(campus_count) {
     var i;
     var result_html = '';
     var campus_container = document.getElementById('campus-names-input-wrapper');
+    var campus_count_number;
     campus_container.innerHTML = '';
     for (i = 0; i < campus_count; i++) {
-        result_html += `
-            <div class="row mt1 campus-name-fade-in">
-                <div class="col-4 centerv npl">
-                    Name of Campus ${i + 1}:
-                </div>
-                <div class="col-8">
-                    <input name="campus_name_${i + 1}" id="campus_name_${i + 1}" type="text" placeholder="•••••••••••••••••"/>
-                </div>
-            </div>`
+        campus_count_number += 1;
+        result_html +=
+            '<div class="row mt1 campus-name-fade-in">' +
+                '<div class="col-4 centerv npl">' +
+                    'Name of Campus ' + campus_count_number.toString() +
+                '</div>' +
+                '<div class="col-8">' +
+                    '<input name="campus_name_' + campus_count_number.toString() + '" ' +
+                    'id="campus_name_' + campus_count_number.toString() + '" ' +
+                    'type="text" placeholder="•••••••••••••••••"/>' +
+                '</div>' +
+            '</div>'
     }
     campus_container.innerHTML = result_html;
 }
