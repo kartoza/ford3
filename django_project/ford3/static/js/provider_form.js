99759435533875cedd683f2729c96e6cb4512346
Christiaanvdm <Christiaan@kartoza.com>
2019-04-02 16:14:29 +0200

    Added basic js to remove + add campus input fields

--- django_project/ford3/static/js/provider_form.js
+++ django_project/ford3/static/js/provider_form.js
@@ -1,17 +1,46 @@
-var output = document.getElementById('number-of-campuses');
-// Update the current slider value (each time you drag the slider handle)
-output.onchange = function () {
-    createCampusNameInputs(output.value);
-}
+ $('.remove_campus_button').click(function() {
+        this_button_parrent_parent = $(this).parent().parent();
+        this_button_parrent_parent.remove();
+    })
 
 $(document).on("submit", "form", function(e) {
     if (confirm('Submitting data. Continue?')) {
+        campus_name_elements = $('.campus_name').elements
+
        return true;
     }
     else {
        return false;
     }
 })
+
+$('#add-campus-name').click(
+    function() {addCampusNameInput()}
+    )
+
+function addCampusNameInput() {
+    var campus_container = document.getElementById('campus-names-input-wrapper');
+
+    var result_html =
+            '<div class="row mt1 campus-name-fade-in">' +
+                '<div class="col-4 centerv npl">' +
+                    'Campus Name ' +
+                '</div>' +
+                '<div class="col-7">' +
+                    '<input required class="campus_name" ' +
+                    'type="text" placeholder="•••••••••••••••••"/>' +
+                '</div>' +
+                '<div class="col-1">' +
+                    '<div class="edu-button edu-button-orange remove_campus_button">X</div>' +
+                '</div>' +
+            '</div>'
+
+    campus_container.innerHTML += result_html
+    $('.remove_campus_button').click(function() {
+        this_button_parrent_parent = $(this).parent().parent();
+        this_button_parrent_parent.remove();
+    })
+}
 
 function createCampusNameInputs(campus_count) {
     var i;
@@ -31,6 +60,9 @@ function createCampusNameInputs(campus_count) {
                     'id="campus_name_' + campus_count_number.toString() + '" ' +
                     'type="text" placeholder="•••••••••••••••••"/>' +
                 '</div>' +
+            '</div>' +
+            '<div class="col-1">' +
+                '<div class="edu-button edu-button-orange remove_campus_button">X</div>' +
             '</div>'
     }
     campus_container.innerHTML = result_html;
