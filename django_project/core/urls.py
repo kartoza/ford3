76a87b561a4e0f62b69064006b96288626599afe
Boney Bun <boney.bun@gmail.com>
2019-05-17 14:01:47 +0700

    add auth url into core

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.shortcuts import render
 from sentry_sdk import capture_message
@@ -41,6 +42,8 @@ urlpatterns = [
     url(r'^site-admin/', admin.site.urls),
     url(r'^ford3/', include('ford3.urls')),
     url(r'^', include('base.urls')),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     # url(r'^', include('ford3.urls')),
     # url(r'^accounts/', include('allauth.urls')),
 ]
