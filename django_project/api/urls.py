88b854c0b212e953bf6fa81d4f20264ff7acbb4f
Christiaanvdm <Christiaan@kartoza.com>
2019-05-30 14:25:59 +0200

    Added a basic test

--- django_project/api/urls.py
+++ django_project/api/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from rest_framework import routers
 from api.viewsets.provider import ProviderViewSet
-
-router = routers.SimpleRouter()
-router.register(r'^(?P<version>(v1))/providers', ProviderViewSet)
-urlpatterns = router.urls
+#
+# router = routers.SimpleRouter()
+# router.register(
+#     r'^(?P<version>(v1))/providers',
+#     ProviderViewSet,
+#     'show-providers-api')
+urlpatterns = [
+    url(r'^(?P<version>(v1))/providers',
+        ProviderViewSet.as_view({'get': 'list'}),
+        name='show-providers-api')
+]
