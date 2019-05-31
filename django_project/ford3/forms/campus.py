562b7fdd4eabb2865fc1c146ef8535d66a3aeee9
Boney Bun <boney.bun@gmail.com>
2019-05-31 15:59:01 +0700

    fix flake8

--- django_project/ford3/forms/campus.py
+++ django_project/ford3/forms/campus.py
@@ -21,7 +21,8 @@ class CampusDetailForm(CampusForm):
 
     telephone = forms.CharField(
         label='Telephone number',
-        widget=forms.TextInput(attrs={'placeholder': '+271234567890 or 123456789012345'}),
+        widget=forms.TextInput(
+            attrs={'placeholder': '+271234567890 or 123456789012345'}),
         required=False)
 
     email = forms.EmailField(
