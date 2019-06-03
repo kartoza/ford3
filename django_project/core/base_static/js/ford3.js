cb5407928b29135a8002655d587f4fc832eab56a
Boney Bun <boney.bun@gmail.com>
2019-06-03 13:27:46 +0700

    Add responsive design to site

--- django_project/core/base_static/js/ford3.js
+++ django_project/core/base_static/js/ford3.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function() {
+    $('#sidebar-small-link').click(function () {
+        let stm = $('.small-sidebar-menu');
+        if (stm.hasClass('show-sidebar-menu')) {
+            stm.removeClass('show-sidebar-menu')
+        }
+        else {
+            stm.addClass('show-sidebar-menu');
+        }
+    });
+});
