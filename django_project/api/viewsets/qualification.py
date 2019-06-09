e8328e96e047befbd9cec7b76203eea5bb39a7d9
Christiaanvdm <Christiaan@kartoza.com>
2019-06-09 22:04:41 +0200

    WIP

--- django_project/api/viewsets/qualification.py
+++ django_project/api/viewsets/qualification.py
@@ -4,5 +4,12 @@ from api.serializers.qualification import QualificationSerializer
 
 
 class QualificationViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    retrieve:
+    Return all details for a qualification specified by ID.
+
+    list:
+    Returns a list of all qualifications registered with OpenEdu.
+    """
     queryset = Qualification.active_objects.all()
     serializer_class = QualificationSerializer
--- django_project/api/viewsets/saqa_qualification.py
+++ django_project/api/viewsets/saqa_qualification.py
@@ -4,5 +4,12 @@ from api.serializers.saqa_qualification import SAQAQualificationSerializer
 
 
 class SAQAQualificationViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    retrieve:
+    Returns a SAQA qualification for a given ID.
+
+    list:
+    Returns a list of all SAQA qualifications registered with OpenEdu.
+    """
     queryset = SAQAQualification.objects.all()
     serializer_class = SAQAQualificationSerializer
