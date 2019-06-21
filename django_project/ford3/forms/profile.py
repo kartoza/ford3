42402e73733c2b1dc9a6633dff557a508e5e0f00
Christiaanvdm <Christiaan@kartoza.com>
2019-06-21 11:10:05 +0200

    Updated layout to use crispy forms

--- django_project/ford3/forms/profile.py
+++ django_project/ford3/forms/profile.py
@@ -1,7 +1,18 @@
 from django import forms
+from crispy_forms.helper import FormHelper
 
 
 class ProfileForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-3'
+        self.helper.field_class = 'col-md-9'
+        self.helper.form_error_title = 'Form Errors'
+        self.helper.help_text_inline = True
+
     first_name = forms.CharField(
         label='First Name',
         widget=forms.TextInput(
